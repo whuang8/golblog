d07816dd29c2072f84cb98938e9fe43de9792112
whuang8 <huang554@purdue.edu>
2016-07-20 19:01:08 -0500

    Add update action to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -15,6 +19,17 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully edited"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+
   end
 
 
